3d1b81787caa25ce02478febee47af399084f264
jsssca <jsbibby@proton.me>
2022-11-15 22:12:42 +0000

    api key

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ const location = 'London,UK'
 const req = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + location + '?unitGroup=metric&key=' + process.env.WEATHER_API_KEY + '&contentType=json';
 
 module.exports = {
-    weather_req: req
+    weather_req: 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?unitGroup=metric&key=K24HG5QFMFK5BQQHG6K3JVVTP&contentType=json'
 }
